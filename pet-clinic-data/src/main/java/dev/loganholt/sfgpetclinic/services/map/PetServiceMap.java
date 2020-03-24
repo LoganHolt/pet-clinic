4258d265632088d481a34e00a395269dbb270259
Logan Holt <loganholtdev@gmail.com>
2020-03-24 17:04:39 -0400

    Implemented Map Based Services, Closes #6

---
+++ pet-clinic-data/src/main/java/dev/loganholt/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package dev.loganholt.sfgpetclinic.services.map;
+
+import dev.loganholt.sfgpetclinic.model.Pet;
+import dev.loganholt.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
