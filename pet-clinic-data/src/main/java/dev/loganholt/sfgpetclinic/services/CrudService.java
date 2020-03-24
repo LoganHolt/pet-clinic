d96b40bd7256911209a8bebb4d4a3ae259aa26b5
Logan Holt <loganholtdev@gmail.com>
2020-03-24 15:14:38 -0400

    Refactoring to common crud interface. Closes #11

---
+++ pet-clinic-data/src/main/java/dev/loganholt/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,12 @@
+package dev.loganholt.sfgpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+    T findById(Long id);
+    T save(T object);
+    void delete(T object);
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/dev/loganholt/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/dev/loganholt/sfgpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package dev.loganholt.sfgpetclinic.services;
 
 import dev.loganholt.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
